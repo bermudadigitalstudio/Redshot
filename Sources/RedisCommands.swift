393e576828779fdb99c9bb4a61a83b15ab8fefe4
Laurent Gaches <laurent@binimo.com>
2017-06-13 18:19:33 +0200

    some documentations

--- Sources/RedisCommands.swift
+++ Sources/RedisCommands.swift
@@ -17,8 +17,29 @@ extension Redis {
         return try sendCommand("GET \(key)")
     }
 
-    public func set(key: String, value: String) throws -> RedisType {
-        return try sendCommand("SET \(key) \(value)")
+
+    /// Set key to hold the string value. If key already holds a value, it is overwritten, regardless of its type.
+    /// Any previous time to live associated with the key is discarded on successful SET operation.
+    ///
+    /// - Parameters:
+    ///   - key: The key.
+    ///   - value: The value to set
+    ///   - exist: if true Only set the key if it already exist. if false Only set the key if it does not already exist.
+    ///   - expire: If not nil, set the specified expire time, in milliseconds.
+    /// - Returns: A simple string reply OK if SET was executed correctly.
+    /// - Throws:
+    public func set(key: String, value: String, exist: Bool? = nil, expire: TimeInterval? = nil) throws -> RedisType {
+        var cmd = "SET \(key) \(value)"
+
+        if let exist = exist {
+            cmd.append(exist ? "XX" : "NX")
+        }
+
+        if let expire = expire {
+            cmd.append("PX \(Int(expire * 1000.0))")
+        }
+
+        return try sendCommand(cmd)
     }
 
     public func sadd(key: String, values: String...) throws -> RedisType {
